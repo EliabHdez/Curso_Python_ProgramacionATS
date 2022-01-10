4f57f8c1c57428cb134ced7a5c25d7ee76139878
eliab_hdez <moises_hl_zod@hotmail.com>
2022-01-09 23:41:08 -0600

    Inicio / Fin del tema Primer Hola Mundo. Creacion del archivo py Primer HolaMundo

---
+++ Primer_HolaMundo.py
@@ -0,0 +1,11 @@
+# Introduccion a Python
+
+# Elementos básicos
+
+print('Hola Mundo')
+
+print('Mi nombre es Moises')
+
+# Si queremos hacer un solo print pero con un respectivo salto de linea para dividir los mensajes los podemos hacer con el caracter "/n" que representa eso, un salto de linea
+
+print('Hello World\nMi nombre es Eliab Hernandez')
