4b54c20e33209529678d5dca2cc40431df046167
eliab_hdez <moises_hl_zod@hotmail.com>
2022-01-11 21:00:40 -0600

    Inicio / Fin del tema Comentarios. Creacion del archivo py Comentarios

---
+++ Comentarios.py
@@ -0,0 +1,17 @@
+""" Los comentarios son porciones en el codigo, los cuales el programa no lee al momento de correr. Son anotaciones que el programa ignora y estas solo sirven como referencua y ayuda. SIRVE PARA DOCUMENTAR EL CODIGO
+
+Hay dos tipos de comentarios.
+    - Comentarios de una sola linea
+    - Comentarios de bloque o multilinea
+    
+Los comentarios de una sola linea como su nombre lo indica, se generan en una sola linea, al momento de hacer un salto de linea se pierde el comentario
+
+Los comentarios de bloque nos permiten generar comentarios mas amplios teniendo saltos de linea y escribiendo en cualquier parte siempre y cuando se encuentren dentro del espacio del comentario
+
+Linea --> Se generan con el signo de numero (#) al inicio de la linea o comentario
+Bloque / Multilinea --> Se generan con comillas, ya sean simples o dobles. 3 comillas al inicio y 3 comillas al final en caso de ser comillas simples, si son comillas dobles son 3 juegos de comillas al inicio y 3 juegos al final, quedando de esta manera (''' comentario ''')"""
+
+valor = 10 # Esta es una variable
+
+print(valor) # Este es un print el cual sirve para mostrar o imprimir el contenido de sus parentesis
+print(type(valor))
