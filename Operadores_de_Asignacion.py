7534d648779952cf76a75bf85fe411b268eada62
eliab.hdez <moises_hl_zod@hotmail.com>
2022-01-15 14:55:58 -0600

    Inicio / Fin del tema Operadores de Asignacion. Creacion del archivo Operadores_de_Asignacion.py

---
+++ Operadores_de_Asignacion.py
@@ -0,0 +1,35 @@
+# Operadores de Asignacion
+
+# Estos operadores nos sirven para escribir codigo mas rapido y no tan largo
+
+""" Los operadores de asignacion son los siguientes:
+        += --> Suma
+        -= --> Resta
+        *= --> Multiplicacion
+        /= --> Division
+        **= --> Potencia
+        %= --> Modulo
+ """
+
+a = 0
+print(a)
+
+# Supongamos que queremos aumentarle a la variable a su valor en 5, una forma de hacerlo seria... a = a + 5 y esta bien, pero es muy redundante, para eso tenemos los operadores de asignacion, lo cual utilizando estos ultimos quedaria de la siguiente manera
+
+a += 5 # Suma en asignacion
+print(a)
+
+a -= 2 # Resta en asignacion
+print(a)
+
+a *= 3 # Multiplicacion en asignacion
+print(a)
+
+a /= 2 # Division en asignacion
+print(a)
+
+a **= 2 # Potencia en asignacion
+print(a)
+
+a %= 2 # Modulo de asignacion
+print(a)
